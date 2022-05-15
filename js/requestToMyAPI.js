e83e3474b6ede370dec11e24d6fbda558e61d53f
GrishaginEvgeny <thenoydota@gmail.com>
2022-05-15 20:03:19 +0300

    finish

--- js/requestToMyAPI.js
+++ js/requestToMyAPI.js
@@ -1,23 +1,23 @@
 document.forms.metro.elements.button.addEventListener('click', ()=>
 {
     let form = new FormData();
-    form.append('inputedAddress', document.forms.metro.elements.address)
-    fetch('yandex.php', {
+    form.append('inputedAddress', document.forms.metro.elements.address.value)
+    fetch('/yandex.php', {
         method: 'POST',
         body: form,
-    }).then((response) => {return response.json()}).then((jsonedResponse)=>{
-        document.getElementById("response").classList.remove('invalid-feedback', 'valid-feedback')
-        if(jsonedResponse["status"]){
-
-            document.getElementById("response").classList.add('valid-feedback');
-            document.getElementById("response").innerHTML = "<p>Адрес: ${jsonedResponse.fullAddress}</p>";
-            document.getElementById("response").innerHTML += "<p>Широта: ${jsonedResponse.latitude}</p>";
-            document.getElementById("response").innerHTML += "<p>Долгота: ${jsonedResponse.longitude}</p>";
-            document.getElementById("response").innerHTML += "<p>Метро: ${jsonedResponse.metro}</p>";
-        }
-        else{
-            document.getElementById("response").classList.add('invalid-feedback');
-            document.getElementById("response").innerHTML = "<p>Поблизости нет метро.</p>";
-        }
-    })
+    }).then((response) => response.json()).then((jsonedResponse)=>{
+            document.getElementById("response").classList.remove('invalid-feedback', 'valid-feedback')
+            document.getElementById("addr").innerHTML = `<p>Адрес: ${jsonedResponse.fullAddress}</p>`;
+            document.getElementById("addr").innerHTML += `<p>Широта: ${jsonedResponse.latitude}</p>`;
+            document.getElementById("addr").innerHTML += `<p>Долгота: ${jsonedResponse.longitude}</p>`;
+            if (jsonedResponse.status === true) {
+                document.getElementById("response").classList.add('valid-feedback');
+                document.getElementById("response").innerHTML = `<p>Метро: ${jsonedResponse.metro}</p>`;
+            } else {
+                document.getElementById("response").classList.add('invalid-feedback');
+                document.getElementById("response").innerHTML = `<p>Поблизости нет метро.</p>`;
+            }
+    }).catch((e)=>{document.getElementById("addr").innerHTML = ``;
+        document.getElementById("response").classList.add('invalid-feedback');
+        document.getElementById("response").innerHTML = `Ошибка: вы ввели несущестующий адрес или адрес в неправильном формате`;})
 })
--- yandex.php
+++ yandex.php
@@ -1,1 +1,52 @@
 <?php
+
+$params = [
+    'apiKey' => parse_ini_file("config/params.ini", true)['apiKey'],
+    'inputedAddress' => htmlspecialchars($_POST['inputedAddress']),
+    'format' => 'json',
+];
+$result = array();
+$curl_settings = curl_init();
+
+
+curl_setopt($curl_settings,
+    CURLOPT_URL , sprintf("https://geocode-maps.yandex.ru/1.x?geocode=%s&apikey=%s&format=%s&results=1",
+        rawurlencode($params['inputedAddress']), $params['apiKey'], $params['format']));
+curl_setopt($curl_settings,
+    CURLOPT_RETURNTRANSFER , 1);
+
+
+$response_for_address_on_inputed_data = curl_exec($curl_settings);
+curl_close($curl_settings);
+    $address_data = json_decode($response_for_address_on_inputed_data, 1);
+
+    $coords = explode(" ", $address_data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]);
+    $params['coords'] = $coords[0] . ',' . $coords[1];
+    $result['latitude'] = $coords[1];
+    $result['longitude'] = $coords[0];
+    $result['fullAddress'] = $address_data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text'];
+
+    $new_curl_settings = curl_init();
+    curl_setopt($new_curl_settings,
+        CURLOPT_URL, sprintf("https://geocode-maps.yandex.ru/1.x?geocode=%s&apikey=%s&format=%s&results=1&kind=metro",
+            rawurlencode($params['coords']), $params['apiKey'], $params['format']));
+    curl_setopt($new_curl_settings,
+        CURLOPT_RETURNTRANSFER, 1);
+
+    $metro_response = curl_exec($new_curl_settings);
+
+    curl_close($new_curl_settings);
+
+    $metro_data = json_decode($metro_response, 1);
+
+
+
+
+    if($metro_data["response"]["GeoObjectCollection"]["featureMember"] == null) {
+        $result["status"] = false;
+    } else {
+        $result["status"] = true;
+        $result['metro'] = $metro_data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["name"];
+    }
+
+echo json_encode($result);
